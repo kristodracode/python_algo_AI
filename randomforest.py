4f9936a5a8e8a62444fcfcb0032a6b76da845fb1
yogeshahuja21 <47299524+yogeshahuja21@users.noreply.github.com>
2019-06-13 20:47:30 +0530

    Add files via upload

---
+++ randomforest.py
@@ -0,0 +1,6 @@
+from sklearn.tree import export_graphviz
+from sklearn.datasets import load_breast_cancer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
